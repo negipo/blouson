52afc2519f8b0b1b2e6881708db86f0bf28c9479
Yoshiteru Negishi <yoshiteru-negishi@locoguide.co.jp>
2024-06-12 10:28:08 +0900

    Fix signature in more compatible style

--- lib/blouson/sensitive_table_query_log_silencer.rb
+++ lib/blouson/sensitive_table_query_log_silencer.rb
@@ -1,13 +1,13 @@
 module Blouson
   class SensitiveTableQueryLogSilencer < Arproxy::Base
-    def execute(sql, name=nil, **kwargs)
+    def execute(sql, *, **)
       if Rails.logger.level != Logger::DEBUG || !(Blouson::SENSITIVE_TABLE_REGEXP === sql)
-        return super(sql, name, **kwargs)
+        return super
       end
 
       ActiveRecord::Base.logger.silence(Logger::INFO) do
         Rails.logger.info "  [Blouson::SensitiveTableQueryLogSilencer] SQL Log is skipped for sensitive table"
-        super(sql, name, **kwargs)
+        super
       end
     end
   end
