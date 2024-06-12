81cd43f1d8e625766ec5c29501ffe9885d6fb039
Yoshiteru Negishi <yoshiteru-negishi@locoguide.co.jp>
2024-06-12 10:29:41 +0900

    Patch more methods for exec_query / internal_exec_query

--- lib/blouson/sensitive_table_query_log_silencer.rb
+++ lib/blouson/sensitive_table_query_log_silencer.rb
@@ -10,5 +10,27 @@ module Blouson
         super
       end
     end
+
+    def exec_query(sql, *, **)
+      if Rails.logger.level != Logger::DEBUG || !(Blouson::SENSITIVE_TABLE_REGEXP === sql)
+        return super
+      end
+
+      ActiveRecord::Base.logger.silence(Logger::INFO) do
+        Rails.logger.info "  [Blouson::SensitiveTableQueryLogSilencer] SQL Log is skipped for sensitive table"
+        super
+      end
+    end
+
+    def internal_exec_query(sql, *, **)
+      if Rails.logger.level != Logger::DEBUG || !(Blouson::SENSITIVE_TABLE_REGEXP === sql)
+        return super
+      end
+
+      ActiveRecord::Base.logger.silence(Logger::INFO) do
+        Rails.logger.info "  [Blouson::SensitiveTableQueryLogSilencer] SQL Log is skipped for sensitive table"
+        super
+      end
+    end
   end
 end
